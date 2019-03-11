13c93ad80f38d42512dfb8ccafe559eb87eb0e36
root <root@ip-172-31-26-75.ap-south-1.compute.internal>
2019-03-11 13:28:28 +0000

    first Comit

---
+++ src/main/java/kranthi/App.java
@@ -0,0 +1,13 @@
+package kranthi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
